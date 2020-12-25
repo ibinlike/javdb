a1cfa3c2f4b01a88463f0c1f61524d879169c31f
ibinlike <ibinlike@gmail.com>
2020-12-25 09:54:51 +0800

    'update download code'

--- source/javdb/javdb/download.py
+++ source/javdb/javdb/download.py
@@ -30,38 +30,47 @@ def load_download_information(file_path,downloaded_path):
     if os.path.isfile(downloaded_id_path):
         with jsonlines.open(downloaded_id_path) as reader:
             downloaded_id = [id for id in reader]
+            print(downloaded_id)
         
     else:
          downloaded_id =[]
     with jsonlines.open(file_path) as reader:
         #print('There are total {} download links...'.format(len(reader)))
+        print('load success...')
         new_added = 0
         for item in reader:
+            print(item)
             dt = datetime.strptime(item['movie_meta'], '%m/%d/%Y')
             if dt.year > 2016:
                 star_save_path = share_dir + str(item['movie_star'])
+                print('start_save_path:', star_save_path)
                 if os.path.isdir(star_save_path):
+                    print('dir is existing...')
                     pass
                 else:
+                    print('dir is not there...')
+                    print('create dir...')
                     os.mkdir(star_save_path)
                 if (item['movie_id'] not in downloaded_id) and ('VR' or 'vr' not in item['movie_title']):
-                    #print(item['movie_img_src'])
+                   # print(item['movie_img_src'])
+                    print('movie is valid, process downloading...')
                     incompleted = True
                     while incompleted:
                         try:
                             img_r = requests.get(item['movie_img_src'], headers = headers)
                             movie_save_path = star_save_path + '/' + str(item['movie_id'])
+                            print('movie_save_path:', movie_save_path)
                             if os.path.isdir(movie_save_path):
                                 pass
                             else:
                                 os.mkdir(movie_save_path)
                             with open(movie_save_path+'/'+'poster.jpg', 'wb') as f:
-                                 f.write(img_r.content)
+                                  f.write(img_r.content)
                             incompleted = False
                             savepath = 'E:\\'+'Download'+'\\'+ str(item['movie_star']) + '\\'+str(item['movie_id'])
+                            print(savepath)
                             qb.download_from_link(item['mega_link'], savepath=savepath, category = item['movie_star'])
                             downloaded_id.append(item['movie_id'])
-                            #print('working...{}'.format(new_added))
                             new_added += 1
                             time.sleep(10)
                         except:
